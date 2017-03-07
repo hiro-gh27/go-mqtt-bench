e9eed2d26e9a6f408e7753199b8a68eceb26c8fb
hiro-gh27 <hiroki@akimac30.kyoto-su.ac.jp>
2017-03-07 14:50:58 +0900

    コード整理

--- go-mqtt-bench.go
+++ go-mqtt-bench.go
@@ -24,53 +24,28 @@ func main() {
 	initSeed()
 
 	var clients []MQTT.Client
-	clientsNum := 100
-	//ch := make(chan MQTT.Client)
-	//create a ClientOptions struct setting the broker address, clientid, turn
-	//off trace output and set the default message handler
+	clientsNum := 10
+	ch := make(chan MQTT.Client)
 	opts := MQTT.NewClientOptions().AddBroker("tcp://localhost:1883")
 	opts.SetDefaultPublishHandler(f)
 	opts.SetCleanSession(false)
 	for i := 0; i < clientsNum; i++ {
 		//clientID := randomString(10)
-		c := randomConnect(opts, i)
+		c := randomConnect(opts, i, ch)
 		clients = append(clients, c)
 	}
 
-	time.Sleep(10000 * time.Millisecond)
+	for index := 0; index < clientsNum; index++ {
+
+	}
+
+	//time.Sleep(10000 * time.Millisecond)
 
 	for _, c := range clients {
 		periodPublish(c)
 		c.Disconnect(250)
+		time.Sleep(300 * time.Millisecond)
 	}
-
-	/**
-
-	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
-	//at a maximum qos of zero, wait for the receipt to confirm the subscription
-	if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
-	//from the server after sending each message
-	for i := 0; i < 5; i++ {
-		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish("go-mqtt/sample", 0, false, text)
-		token.Wait()
-	}
-
-	time.Sleep(3 * time.Second)
-
-	//unsubscribe from /go-mqtt/sample
-	if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-
-	c.Disconnect(250)
-	*/
 }
 
 //use random
@@ -80,18 +55,11 @@ func initSeed() {
 
 //cheak!! (シグナルを受け取るまで回し続けるっていうアイデア)
 func periodPublish(c MQTT.Client) {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1; i++ {
 		text := fmt.Sprintf("this is message")
 		token := c.Publish("go-mqtt/sample", 0, false, text)
 		token.Wait()
 	}
-	/**
-	for i := 0; i < 1; i++ {
-		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish("go-mqtt/sample", 0, false, text)
-		token.Wait()
-	}
-	*/
 }
 
 //makeの理解がいまいち, もっと効率のいい生成があるっぽい.
@@ -103,7 +71,7 @@ func randomString(n int) string {
 	return string(b)
 }
 
-func randomConnect(opts *MQTT.ClientOptions, i int) MQTT.Client {
+func randomConnect(opts *MQTT.ClientOptions, i int, ch chan MQTT.Client) MQTT.Client {
 	/*waitTime := */ //randomInterval()
 	clientID := "clientID: "
 	clientID += strconv.FormatInt(int64(i), 10)
@@ -117,6 +85,7 @@ func randomConnect(opts *MQTT.ClientOptions, i int) MQTT.Client {
 		panic(token.Error())
 	}
 	//periodPublish(c)
+
 	return c
 }
 
@@ -127,3 +96,30 @@ func randomInterval() int {
 }
 
 // /usr/local/opt/mosquitto/sbin/mosquitto
+/**
+
+//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+//at a maximum qos of zero, wait for the receipt to confirm the subscription
+if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+	fmt.Println(token.Error())
+	os.Exit(1)
+}
+
+//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+//from the server after sending each message
+for i := 0; i < 5; i++ {
+	text := fmt.Sprintf("this is msg #%d!", i)
+	token := c.Publish("go-mqtt/sample", 0, false, text)
+	token.Wait()
+}
+
+time.Sleep(3 * time.Second)
+
+//unsubscribe from /go-mqtt/sample
+if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+	fmt.Println(token.Error())
+	os.Exit(1)
+}
+
+c.Disconnect(250)
+*/

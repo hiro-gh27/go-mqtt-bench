e4fe18a0316929b2f6bd42e19fc11cd9de9b6b72
hiro-gh27 <hiroki@akimac30.kyoto-su.ac.jp>
2017-03-08 14:48:55 +0900

    commit before making randomStr

--- go-mqtt-bench.go
+++ go-mqtt-bench.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"runtime"
 	"strconv"
 	"sync"
-
-	"bytes"
-
 	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
@@ -36,9 +34,11 @@ func execute(opts execOptions) {
 		client := connect(index, opts)
 		clients = append(clients, client)
 	}
+	//安定せるために少し待つらしい. 3000は適当です :-)
 	time.Sleep(3000 * time.Millisecond)
 
 	startTime := time.Now()
+	fmt.Println("start")
 	publishRequestAll(clients, opts)
 	endTime := time.Now()
 	duration := (endTime.Sub(startTime)).Nanoseconds() / int64(1000000)
@@ -52,7 +52,7 @@ func asyncDisconnect(clients []MQTT.Client) {
 	for _, c := range clients {
 		wg.Add(1)
 		go func(client MQTT.Client) {
-			client.Disconnect(10)
+			client.Disconnect(250)
 			wg.Done()
 		}(c)
 	}
@@ -71,7 +71,7 @@ func connect(id int, execOpts execOptions) MQTT.Client {
 	client := MQTT.NewClient(opts)
 	token := client.Connect()
 	if token.Wait() && token.Error() != nil {
-		panic(token.Error())
+		fmt.Printf("Connected error: %s\n", token.Error())
 	}
 	return client
 }
@@ -100,8 +100,8 @@ func publishRequestAll(clients []MQTT.Client, opts execOptions) int {
 				//fmt.Printf("Publish : id=%d, count=%d, topic=%s\n", clientID, index, topic)
 				token.Wait()
 			}
-			//sys := fmt.Sprintf("finished ClientID = %d", clientID)
-			//fmt.Println(sys)
+			sys := fmt.Sprintf("finished ClientID = %d", clientID)
+			fmt.Println(sys)
 			wg.Done()
 		}(id)
 	}
@@ -110,124 +110,16 @@ func publishRequestAll(clients []MQTT.Client, opts execOptions) int {
 }
 
 func main() {
+	//デフォルトでcpuは4つ使うのかな...??
 	cpus := runtime.NumCPU()
 	println(cpus)
 	runtime.GOMAXPROCS(cpus)
 	execOpts := execOptions{}
-	execOpts.Broker = "tcp://localhost:1883"
-	execOpts.ClientNum = 1
+	execOpts.Broker = "tcp://169.254.120.135:1883"
+	execOpts.ClientNum = 100
 	execOpts.Qos = 0
-	execOpts.Count = 100000
+	execOpts.Count = 10
 	execOpts.Topic = "go-mqtt/"
 	execute(execOpts)
 
 }
-
-/*
-var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-const rs2Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-//define a function for the default message handler
-var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
-}
-
-//use random
-func initSeed() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-//cheak!! (シグナルを受け取るまで回し続けるっていうアイデア)
-func periodPublish(c MQTT.Client) {
-	for i := 0; i < 1; i++ {
-		text := fmt.Sprintf("this is message")
-		token := c.Publish("go-mqtt/sample", 0, false, text)
-		token.Wait()
-	}
-}
-
-//makeの理解がいまいち, もっと効率のいい生成があるっぽい.
-func randomString(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = rs1Letters[rand.Intn(len(rs1Letters))]
-	}
-	return string(b)
-}
-
-func randomConnect(opts *MQTT.ClientOptions, i int, ch chan MQTT.Client) MQTT.Client {
-	//randomInterval()
-	clientID := "clientID: "
-	clientID += strconv.FormatInt(int64(i), 10)
-	opts.SetClientID(clientID)
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	//periodPublish(c)
-	return c
-}
-
-func randomInterval() int {
-	waitTime := rand.Intn(1000)
-	time.Sleep(time.Duration(waitTime) * time.Millisecond)
-	return waitTime
-}
-
-func main() {
-	initSeed()
-
-	var clients []MQTT.Client
-	clientsNum := 10
-	ch := make(chan MQTT.Client)
-	opts := MQTT.NewClientOptions().AddBroker("tcp://localhost:1883")
-	opts.SetDefaultPublishHandler(f)
-	opts.SetCleanSession(false)
-	for i := 0; i < clientsNum; i++ {
-		//clientID := randomString(10)
-		c := randomConnect(opts, i, ch)
-		clients = append(clients, c)
-	}
-
-	for index := 0; index < clientsNum; index++ {
-
-	}
-
-	for _, c := range clients {
-		periodPublish(c)
-		c.Disconnect(250)
-		time.Sleep(300 * time.Millisecond)
-	}
-}
-*/
-// /usr/local/opt/mosquitto/sbin/mosquitto
-/**
-
-//subscribe to the topic /go-mqtt/sample and request messages to be delivered
-//at a maximum qos of zero, wait for the receipt to confirm the subscription
-if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
-	fmt.Println(token.Error())
-	os.Exit(1)
-}
-
-//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
-//from the server after sending each message
-for i := 0; i < 5; i++ {
-	text := fmt.Sprintf("this is msg #%d!", i)
-	token := c.Publish("go-mqtt/sample", 0, false, text)
-	token.Wait()
-}
-
-time.Sleep(3 * time.Second)
-
-//unsubscribe from /go-mqtt/sample
-if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
-	fmt.Println(token.Error())
-	os.Exit(1)
-}
-
-c.Disconnect(250)
-*/
